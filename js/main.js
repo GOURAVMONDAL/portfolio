a20c82bc288b26074d68d4cf757158c00dac0999
GOURAVMONDAL <gouravmondal805@gmail.com>
2020-04-30 18:41:03 +0530

    portfolio-update

--- js/main.js
+++ js/main.js
@@ -43,3 +43,17 @@ barba.init({
 
 // Isotope gallery
 
+
+
+var $grid = $('.portfolio__item').isotope({
+    itemSelector:'.item',
+    layoutMode:'fitRows'
+})
+
+$('.filter-button-group').on('click','li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({filter:filterValue});
+
+    $('.filter-button-group li').removeClass('active');
+    $(this).addClass('active');
+})
