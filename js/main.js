4b47679403176c31e58b5189d80b6909d0377ab5
GOURAVMONDAL <gouravmondal805@gmail.com>
2020-05-05 01:40:36 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -12,6 +12,71 @@ function navAnimation() {
 }
 
 function loadAnimation(){
-    gsap.to('.lines .line',{scaleX:1,transformOrigin:'left',duration:1,stagger:.2,})
-    gsap.to('.lines .line',{scaleX:0,transformOrigin:'right',duration:1,stagger:.2,delay:1})
+    gsap.to('.lines .line',{scaleX:1,transformOrigin:'left',duration:.5,stagger:.1})
+    gsap.to('.lines .line',{scaleX:0,transformOrigin:'right',duration:.5,stagger:.2,delay:1})
 }
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        },n);
+    });
+}
+
+barba.init({
+    sync:true,
+    views:[{
+        namespace:'home',
+        async beforeLeave(data){
+            
+            loadAnimation();
+            await delay(1300);
+        },
+        beforeEnter(data){
+            new Typed('.typing', {
+                strings: ['Web Designer', 'Web Developer','Freelancer'],
+                autoStart: true,
+                loop:true,
+                typeSpeed:100,
+                backSpeed:100,
+            
+              });
+        },
+        before(data){
+            new Typed('.typing', {
+                strings: ['Web Designer', 'Web Developer','Freelancer'],
+                autoStart: true,
+                loop:true,
+                typeSpeed:100,
+                backSpeed:100,
+            
+              });
+        }
+    },{
+        namespace:'contact',
+        async beforeLeave(data){
+            loadAnimation();
+            await delay(1300);
+        }
+    },{
+        namespace:'portfolio',
+        async beforeLeave(data){
+            loadAnimation();
+            await delay(1300);
+        },
+        beforeEnter(data){
+            $('.grid').isotope();
+            $('.portfolio-filter').on('click','li',function(){
+                var filterValue = $(this).attr('data-filter');
+                $('.grid').isotope({filter:filterValue});
+            })
+            
+            $('.portfolio-filter ul li').on('click',function() {
+                $('.portfolio-filter ul li').removeClass('active');
+                $(this).addClass('active');
+            })
+        }
+    }]
+})
