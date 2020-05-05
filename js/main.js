c5568061418a7daa93eb9706de4c77d98f101505
GOURAVMONDAL <gouravmondal805@gmail.com>
2020-05-05 18:34:10 +0530

    final update.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 
 // nav-menu gsap animation
 
-var checkbox = document.querySelector('#check');
+// var checkbox = document.querySelector('#check');
 
-function navAnimation() {
-    if(checkbox.checked == true) {
-        gsap.to('.nav-menu',{x:0,duration:.6,delay:.2,ease:Expo.easeOut})
-    }else{
-        gsap.to('.nav-menu',{x:"100%",duration:.6,ease:Expo.easeOut})
-    }
-}
+// function navAnimation() {
+//     if(checkbox.checked == true) {
+//         gsap.to('.nav-menu',{x:0,duration:.6,delay:.2,ease:Expo.easeOut})
+//     }else{
+//         gsap.to('.nav-menu',{x:"100%",duration:.6,ease:Expo.easeOut})
+//     }
+// }
 
 function loadAnimation(){
     gsap.to('.lines .line',{scaleX:1,transformOrigin:'left',duration:.5,stagger:.1})
